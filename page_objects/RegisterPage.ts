425f51c38b6c69de5eaeb7f3183a979221b3aa75
Issmailduman <https://github.com/Issmailduman>
2023-01-05 22:16:23 +0100

    register page  fatures and steps

---
+++ page_objects/RegisterPage.ts
@@ -0,0 +1,93 @@
+import { expect, Locator, Page } from "@playwright/test";
+import { faker } from '@faker-js/faker';
+
+export class RegisterPage{
+
+registerBtn:Locator;
+registerTable:Locator;
+fName:Locator;
+lName:Locator;
+userEmail:Locator;
+phone:Locator;
+occupationDrop:Locator;
+gender:Locator;
+userPassword:Locator;
+confirmPassword:Locator;
+ageConfirm:Locator;
+registerEnd:Locator;
+studentDrop:Locator;
+successfullyMessage:Locator;
+
+constructor(page:Page){
+this.registerBtn=page.locator('[routerlink="/auth/register"]');
+this.registerTable=page.locator('.login-title');
+this.fName=page.locator('#firstName');
+this.lName=page.locator('#lastName');
+this.userEmail=page.locator('#userEmail');
+this.userPassword=page.locator('#userPassword');
+this.confirmPassword=page.locator('#confirmPassword');
+this.phone=page.locator('#userMobile');
+this.occupationDrop=page.locator('[formcontrolname="occupation"]');
+this.gender=page.locator('[type="radio"]');
+this.ageConfirm=page.locator('[formcontrolname="required"]');
+this.registerEnd=page.locator('#login');
+this.studentDrop=page.locator('value="2: Student"');
+this.successfullyMessage=page.locator('[aria-label="Registered Successfully"]');
+
+
+}
+
+async goToRegisterPage(){
+    await this.registerBtn.click();
+}
+async verifyRegisterPage(){
+await expect (this.registerTable).toHaveText('Register');
+
+}
+async enterEmail(){
+    const email= faker.internet.email();
+    await this.userEmail.fill(email);
+}
+async enterFName(){
+    const firstName=faker.name.firstName();
+    await this.fName.fill(firstName);
+}
+async enterLName(){
+    const lastName=faker.name.lastName();
+    await this.lName.fill(lastName);
+}
+async phoneNmbr(phone_number:string){
+    
+    await this.phone.type(phone_number);
+}
+async selectOccupAtion(){
+    await this.occupationDrop.selectOption("Student");
+    
+}
+async clickGender(){
+    await this.gender.first().click();
+}
+
+async enterPassword(password:string){
+    await this.userPassword.type(password);
+}
+async confirmPass(password:string){
+    await this.confirmPassword.type(password);
+}
+async confirmAge(){
+    await this.ageConfirm.click();
+    console.log(await this.ageConfirm.isChecked());
+    
+    await expect(this.ageConfirm).toBeChecked();
+}
+async registrationEnd(){
+await this.registerEnd.click();
+
+}
+async verifyRegisterSuccessfulL(){
+    await expect(this.successfullyMessage).toHaveText(" Registered Successfully ");
+    const successfullyMessage=await this.successfullyMessage.textContent();
+    console.log(successfullyMessage);
+    
+}
+}
---
+++ steps/registerPage.steps.ts
@@ -0,0 +1,57 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import { page } from "../hooks/world";
+import { LoginPage } from "../page_objects/LoginPage";
+import { RegisterPage } from "../page_objects/RegisterPage";
+
+When('user shoul be able to navi to {string}',async function (string) {
+    const registerPage= new RegisterPage(page);
+    await registerPage.goToRegisterPage();
+  });
+
+
+  Then('user shoul see register table',async function () {
+    const registerPage= new RegisterPage(page);
+   await registerPage.verifyRegisterPage();
+  });
+
+
+
+  When('user enter {string} {string} {string} and {string}',async function (string, string2, string3, phone_number:string) {
+    const registerPage= new RegisterPage(page);
+   await registerPage.enterFName();
+   await registerPage.enterLName();
+   await registerPage.enterEmail();
+   await registerPage.phoneNmbr(phone_number);
+  });
+
+
+
+  When('user sholud be able to select {string} and {string}',async function (string, string2) {
+    const registerPage= new RegisterPage(page);
+   await registerPage.selectOccupAtion();
+   await registerPage.clickGender();
+  });
+
+
+
+  When('user enter the {string} and {string}',async function (password:string, confirm_password:string) {
+    const registerPage= new RegisterPage(page);
+    await registerPage.enterPassword(password);
+    await registerPage.confirmPass(confirm_password);
+  });         
+
+
+
+  When('the user confirm that  is over {int} years old and and completes the registration',async function (int) {
+    const registerPage= new RegisterPage(page);
+ await registerPage.confirmAge();
+  });
+
+
+
+  Then('the user sees the message registration completed successfully',async function () {
+    const registerPage= new RegisterPage(page);
+     await registerPage.registrationEnd();
+     await registerPage.verifyRegisterSuccessfulL();
+  });
