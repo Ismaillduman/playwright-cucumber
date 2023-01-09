90cfd097dec6ad5b7187faf5a6e691c2ad6e325e
Issmailduman <https://github.com/Issmailduman>
2023-01-09 12:33:53 +0100

    try3

--- htmlReportGenerator.ts
+++ htmlReportGenerator.ts
@@ -1,23 +1,30 @@
-import reporter, {Options}from 'cucumber-html-reporter';
-var date =new Date();
+import reporter, { Options } from 'cucumber-html-reporter'
+const date = new Date()
 
-var currentDate=date.getDate()+ '_' + (date.getMonth()+1)+ '_' + date.getFullYear()+ '_'
-+ date.getHours()+'_'+date.getMinutes()+'_'+date.getSeconds()+'_'+date.getMilliseconds();
+const currentDate =
+  date.getDate() +
+  '_' +
+  (date.getMonth() + 1) +
+  '_' +
+  date.getFullYear() +
+  '_' +
+  date.getHours() +
+  '_' +
+  date.getMinutes() +
+  '_' +
+  date.getSeconds() +
+  '_' +
+  date.getMilliseconds()
 
- const options: Options= {
-    
-    
-    brandTitle:"Client App Test Scenarious",
-    theme: "bootstrap",
-    jsonFile: 'test-results/cucumber_report.json',
-    output: 'test-results/cucumber_report_'+currentDate+'.html',
-    screenshotsDirectory:'./Screenshots/',
-    storeScreenshots: true,
-    launchReport:true,
-    reportSuiteAsScenarios: true,
-   
+const options: Options = {
+  brandTitle: 'Client App Test Scenarious',
+  theme: 'bootstrap',
+  jsonFile: 'test-results/cucumber_report.json',
+  output: 'test-results/cucumber_report_' + currentDate + '.html',
+  screenshotsDirectory: './Screenshots/',
+  storeScreenshots: true,
+  launchReport: true,
+  reportSuiteAsScenarios: true,
+}
 
-
-};
-
-reporter.generate(options);
+reporter.generate(options)
